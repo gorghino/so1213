82ec42a6d327a58fd2d62a61a166b5c2426caa5c
gorghino@gmail.com <gorgo@Akira.(none)>
2013-08-27 12:57:20 +0200

    Add const13 customized

---
+++ const13_customized.h
@@ -0,0 +1,15 @@
+/**************************************************************************** 
+ *
+ * This header file contains the global customized constant & macro definitions.
+ * 
+ ****************************************************************************/
+
+#include "base.h"
+
+ /* Interrupting devices bitmaps addresses: the actual bitmap address is
+   computed with INT_INTBITMAP_START + (WORD_SIZE * (int_no - 3)) */
+#define INT_BITMAP_DISKDEVICE 0x1000003c
+#define INT_BITMAP_TAPEDEVICE 0x10000040
+#define INT_BITMAP_NETDEVICE 0x10000044
+#define INT_BITMAP_PRINTERDEVICE 0x10000048
+#define INT_BITMAP_TERMINALDEVICE 0x1000004C
