f98f40944a9e7feacdcc9754dd7f512684adf614
jesky <jesky@jesky-VirtualBox.(none)>
2013-09-01 15:23:28 +0200

    correzione errori e eliminati alcuni warning

--- handler.c
+++ handler.c
@@ -19,7 +19,7 @@
  */
 
 
-//definizione per states_array[6]
+/*definizione per states_array[6]*/ 
  
  #define TLB_OLD 		0
  #define TLB_NEW		1
@@ -66,19 +66,19 @@ void syscallHandler(){
 					addokbuf("CREATEPROCESS\n"); 
 					state_t *child_state;
 					pcb_t *child;
-					//a1 should contain the physical address of a processor state
-					//area at the time this instruction is executed.
-					//This processor state should be used
-					//as the initial state for the newly created process
+					/*a1 should contain the physical address of a processor state
+					area at the time this instruction is executed.
+					This processor state should be used
+					as the initial state for the newly created process*/
 					child_state = (state_t *)(current_process[cpuID]->p_s).reg_a1;
-					//If the new process cannot be created due
-					//to lack of resources (for example no more free ProcBlk’s),
-					// an error code of -1 is
-					//placed/returned in the caller’s v0,
-					// otherwise, return the value 0 in the caller’s v0.
-					//The SYS1 service is requested by the calling process by placing the value
-					//1 in a0, the physical address of a processor state in a1, and then executing a
-					//SYSCALL instruction.
+					/*If the new process cannot be created due
+					to lack of resources (for example no more free ProcBlk’s),
+					 an error code of -1 is
+					placed/returned in the caller’s v0,
+					 otherwise, return the value 0 in the caller’s v0.
+					The SYS1 service is requested by the calling process by placing the value
+					1 in a0, the physical address of a processor state in a1, and then executing a
+					SYSCALL instruction. */
 					if ( ( child = allocPcb() ) != NULL ) {
 						(current_process[cpuID]->p_s).reg_v0 = -1;
 					}
@@ -98,10 +98,10 @@ void syscallHandler(){
 
 				case VERHOGEN: 
 					addokbuf("VERHOGEN\n"); 
-					int *semV = sysBp_old->reg_a1;
+					int *semV = (int*) sysBp_old->reg_a1;
 					(*semV)++;
-					// Se ci sono processi bloccati, il primo viene tolto dalla coda e messo in readyQueue
-					if ((unblocked = removeBlocked(sysBp_old->reg_a1)) != NULL){
+					/* Se ci sono processi bloccati, il primo viene tolto dalla coda e messo in readyQueue*/
+					if ((unblocked = removeBlocked (sysBp_old->reg_a1)) != NULL){
 						softBlock_count[cpuID]--;
 						insertProcQ(&ready_queue, unblocked);
 					}
@@ -109,10 +109,10 @@ void syscallHandler(){
 
 				case PASSEREN: 
 					addokbuf("PASSEREN\n"); 
-					int *semP = sysBp_old->reg_a1;
+					int *semP = (int *) sysBp_old->reg_a1;
 					(*semP)--;
 					if(*semP < 0){
-						// Se il valore del semaforo è negativo, il processo viene bloccato e accodato 
+						/*Se il valore del semaforo è negativo, il processo viene bloccato e accodato */
 						softBlock_count[cpuID]++;
 						insertBlocked(semP, current_process);
 					}
@@ -127,38 +127,38 @@ void syscallHandler(){
 
 					
 					if(!current_process[cpuID]->p_s.reg_a1){
-						//TLB exceptions
-					current_process[cpuID]->states_array[TLB_NEW] = current_process[cpuID]->p_s.reg_a3;
-					current_process[cpuID]->states_array[TLB_OLD] = current_process[cpuID]->p_s.reg_a2;
+						/*TLB exceptions*/
+					current_process[cpuID]->states_array[TLB_NEW] = (state_t*) current_process[cpuID]->p_s.reg_a3;
+					current_process[cpuID]->states_array[TLB_OLD] = (state_t*) current_process[cpuID]->p_s.reg_a2;
 						}
 					else if(current_process[cpuID]->p_s.reg_a1==1){
-						//PGMTRAP exceptions
-						current_process[cpuID]->states_array[PGMTRAP_NEW] = current_process[cpuID]->p_s.reg_a3;
-						current_process[cpuID]->states_array[PGMTRAP_OLD] = current_process[cpuID]->p_s.reg_a2;
+						/*PGMTRAP exceptions*/
+						current_process[cpuID]->states_array[PGMTRAP_NEW] = (state_t*) current_process[cpuID]->p_s.reg_a3;
+						current_process[cpuID]->states_array[PGMTRAP_OLD] = (state_t*) current_process[cpuID]->p_s.reg_a2;
 					}
 					else if (current_process[cpuID]->p_s.reg_a1==2){
-						//SysBp exceptions
-						current_process[cpuID]->states_array[SYSBREAKPOINT_NEW] = current_process[cpuID]->p_s.reg_a3;
-						current_process[cpuID]->states_array[SYSBREAKPOINT_OLD] = current_process[cpuID]->p_s.reg_a2;
+						/*SysBp exceptions*/
+						current_process[cpuID]->states_array[SYSBREAKPOINT_NEW] = (state_t*) current_process[cpuID]->p_s.reg_a3;
+						current_process[cpuID]->states_array[SYSBREAKPOINT_OLD] = (state_t*) current_process[cpuID]->p_s.reg_a2;
 					}
 					break;
                     
 				case GETCPUTIME: 
 					addokbuf("GETCPUTIME\n"); 
-					//current_process[cpuID]->p_s.reg_v0 = (GET_TODLOW() - current_process[cpuID]->startTime);
+					current_process[cpuID]->p_s.reg_v0 = (GET_TODLOW - current_process[cpuID]->startTime);
 					break;
 					
 				case WAITCLOCK: 
 					addokbuf("WAITCLOCK\n"); 
-					//P(pseudo_clock[cpuID], current_process[cpuID]);
+					P(pseudo_clock[cpuID], current_process[cpuID]);
 					break;
 
 				/*int SYSCALL(WAITIO, int intNo, int dnum, int waitForTermRead)	
   					Quando invocata, la SYS8 esegue una P sul semaforo associato al device idenficato da intNo, dnume e waitForTermRead*/
 				case WAITIO: 
 					addokbuf("WAITIO\n"); 
-					//verificare se si è in attesa di I/O
-					/*if (current_process[cpuID]->p_s.reg_a1 == INT_TIMER || INT_LOWEST || INT_DISK || INT_TAPE || 
+					/*verificare se si è in attesa di I/O*/
+					if (current_process[cpuID]->p_s.reg_a1 == INT_TIMER || INT_LOWEST || INT_DISK || INT_TAPE || 
 						INT_UNUSED || INT_PRINTER || INT_TERMINAL) {
 
 						int devNumber = current_process[cpuID]->p_s.reg_a2;
@@ -166,21 +166,24 @@ void syscallHandler(){
 						switch(current_process[cpuID]->p_s.reg_a3){
 							case 0:
 							addokbuf("in scrittura\n");
-							current_process[cpuID]->p_s.reg_v0 = deviceResponseWrite[current->p_s.reg_a2];
+							current_process[cpuID]->p_s.reg_v0 = deviceResponseWrite[current_process[cpuID]->p_s.reg_a2];
 							break;
 
 							case 1:
 							addokbuf("in lettura\n");
-							current_process[cpuID]->p_s.reg_v0 = deviceResponseRead[current->p_s.reg_a2];
+							current_process[cpuID]->p_s.reg_v0 = deviceResponseRead[current_process[cpuID]->p_s.reg_a2];
 							break;
 
 						}
-						P(devNumber,current_process[cpuID]);*/
+						P(devNumber,current_process[cpuID]);
 					break;
 			} 
 			break;
-		case EXC_BREAKPOINT: addokbuf("BREAKPOINT\n"); break;
-	}	
+		
+		}	
+	case EXC_BREAKPOINT: addokbuf("BREAKPOINT\n"); break;
+	}
+
 	sysBp_old->pc_epc += 4; 
 	LDST(sysBp_old); 
 }
